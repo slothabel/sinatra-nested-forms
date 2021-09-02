bfb0fb85a2b191140c98ef0a380b1f16278c5e79
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-09-02 14:40:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,19 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+    
+    post '/pirates' do
+      # @pirate = Pirate.new(params[:pirate])
+
+      erb :show
+    end
 
   end
 end
